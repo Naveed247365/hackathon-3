9e4ee3724cb72a62d967ced5345bd700516c5f71
Naveed247365 <qureshinaveed21@hotmail.com>
2025-02-07 17:08:49 -0800

    update client.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,12 +1,10 @@
-import { createClient } from 'next-sanity'
-
-import { apiVersion, dataset, projectId } from "../env"
+// sanityClient.js
+import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset:process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion:'2025-01-13',
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  token:process.env.NEXT_PUBLIC_SANITY_TOKEN,
-  
-})
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: '2025-01-13',
+  useCdn: true, // Enables the use of Sanity’s CDN for faster responses
+  // Do not include the token here
+});
